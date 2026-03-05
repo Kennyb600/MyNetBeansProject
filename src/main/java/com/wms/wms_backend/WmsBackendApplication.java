bc1bd768cb67eb57af0c35c68deeaf7e4e956be7
Kennyb600 <Kenielbarclay15@gmail.com>
2026-03-04 23:36:56 -0500

    WarrantController and Service created & able to be viewed on browser

--- src/main/java/com/wms/wms_backend/WmsBackendApplication.java
+++ src/main/java/com/wms/wms_backend/WmsBackendApplication.java
@@ -1,13 +1,23 @@
 package com.wms.wms_backend;
 
+import com.wms.wms_backend.service.WarrantService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WmsBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WmsBackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WmsBackendApplication.class, args);
+    }
 
+    // This Bean runs automatically right after the application starts
+    @Bean
+    CommandLineRunner run(WarrantService warrantService) {
+        return args -> {
+            warrantService.createMockCourtData();
+        };
+    }
 }
